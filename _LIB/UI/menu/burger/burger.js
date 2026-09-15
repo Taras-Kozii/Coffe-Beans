c6593fc6be965e9eda6ec9d7f2aa20cf26dc4b6a
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2026-09-15 14:32:42 +0000

    start

--- _LIB/UI/menu/burger/burger.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach(elem => {
-  elem.addEventListener('click', e => {
-    elem.classList.toggle('active');
-  });
-})
--- _LIB/UI/menu/menu.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const menu = document.querySelector('nav.menu__body');
-const burger = document.querySelector('.burger');
-
-burger.addEventListener('click', e => {
-  burger.classList.toggle('active');
-  menu.classList.toggle('open');
-})
